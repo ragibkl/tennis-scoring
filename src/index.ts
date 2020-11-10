7f1a9115f7c6fb83377428a3ad0812c3c9b3b903
Ragib Badaruddin <ragib@seekasia.com>
2020-11-10 16:48:08 +0800

    remove run points

--- src/index.ts
+++ src/index.ts
@@ -5,25 +5,3 @@ const match = new Match()
 const replInstance = repl.start('> ');
 
 Object.assign(replInstance.context, { Match });
-
-match.pointWonBy("player 1");
-match.pointWonBy("player 2");
-// this will return "0-0, 15-15"
-match.score();
-
-match.pointWonBy("player 1");
-match.pointWonBy("player 1");
-// this will return "0-0, 40-15"
-match.score();
-
-match.pointWonBy("player 2");
-match.pointWonBy("player 2");
-// this will return "0-0, Deuce"
-match.score();
-match.pointWonBy("player 1");
-// this will return "0-0, Advantage player 1"
-match.score();
-
-match.pointWonBy("player 1");
-// this will return "1-0"
-match.score();
